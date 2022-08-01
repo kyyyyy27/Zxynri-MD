6cfeec39973d446fcf1f2fd720cf68c7a4f16f4f
kyyyyy27 <107806866+kyyyyy27@users.noreply.github.com>
2022-08-01 19:10:58 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,16 +3,16 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ DaniOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ DaniOffcོ_1ঔৣ꧂
-item1.TEL;waid=6281251549899:6281251549899@s.whatsapp.net
+FN: Yakuzaa
+item.ORG: 𝑆𝐻𝐼𝑅𝑂 
+item1.TEL;waid=6283830815715:6283830815715@s.whatsapp.net
 item1.X-ABLabel:owner selalu ada 
-item2.EMAIL;type=INTERNET: kecedani257@gmail.com
+item2.EMAIL;type=INTERNET: yakuzaakyy@gmail.com
 item2.X-ABLabel:📧Email
 item3.ADR:;;🎍KANDANGAN;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
-item4.URL:https://github.com/Bot-Mdnew
+item4.URL:https://github.com
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
@@ -24,7 +24,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-let dani = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg'
+let dani = 'https://telegra.ph/file/76efbe5bf40a1e2d4c893.jpg'
 await conn.send3ButtonImg(m.chat, dani, "*Hai kak👋,  Itu nomor owner gw…*", '📮: jngn di spam kack', 'Source code', '.sc', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
